e9c5d6af753a1b6d6eeaa91f0e6dd2aec7f9858c
njg7716 <36088815+njg7716@users.noreply.github.com>
2019-04-24 23:45:22 -0400

    Imported Jacks code and cleaned up mine a bit

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python
 
-from packet_parser import *                             #Adds Elliots packet parser
-#When I get Jacks code, import that too
+from packet_parser import parse                                 #Adds Elliots packet parser
+from Packet_Filtering import read                               #When I get Jacks code, import that too
 
 files = ['Node1.txt','Node2.txt','Node3.txt','Node4.txt']       #List of files with the packet data we want
-rqsent = 0
-rqrecv = 0
-rpsent = 0
-rprecv = 0
 
 def main():
     for f in files:
+        rqsent = int(0)
+        rqrecv = int(0)
+        rpsent = int(0)
+        rprecv = int(0)
+        totrqdata = int(0)
+        totrpdata = int(0)
         if f == 'Node1.txt':
             ip = '192.168.100.1'
-        if f == 'Node2.txt':
+        elif f == 'Node2.txt':
             ip == '192.168.100.2'
-        if f == 'Node3.txt':
+        elif f == 'Node3.txt':
             ip = '192.168.200.1'
-        if f == 'Node4.txt':
+        elif f == 'Node4.txt':
             ip == '192.168.200.2' 
-        packetFile= 'blah'                               #pass f through Jacks functions
-        packets = parse(packetFile)                      #Use Jacks output with elliots parse function
+        read(f)                                                 #pass f through Jacks functions
+        packets = parse('goodstuff.txt')                        #Use Jacks output with elliots parse function
         for packet in packets:
             if packet[14] == '8' and packet[12]==ip:
-                rqsent +=1                              #of echo requests sent (type 8)
+                rqsent += 1
+                totrqdata += int(packet[18])                    #of echo requests sent (type 8)
             if packet[14] == '0'and packet[12]==ip: 
-                rpsent +=1                              #of echo replies sent (type 0)
+                rpsent += 1                                     #of echo replies sent (type 0)
+                totrpdata += int(packet[18])
             if packet[14] == '8' and packet[13]==ip:
-                rqrecv+=1                               #of echo requests recieved
+                rqrecv+= 1                                      #of echo requests recieved
+                totrqdata += int(packet[18])
             if packet[14] == '0' and packet[13]==ip:
-                rpsent +=1                              #of echo replies recieved
+                rprecv += 1                                     #of echo replies recieved
+                totrpdata += int(packet[18])
             #Total echo request bytes sent
             #Total echo request bytes recv
-            #Total echo request data sent
-            #Total echo request data recv
         return
+
+if __name__ == '__main__':
+    main()
