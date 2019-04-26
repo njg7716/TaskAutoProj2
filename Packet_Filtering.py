0ae4964a5815c21b3ad4df5cb34d734892fe6bbb
jwb4209 <33587882+jwb4209@users.noreply.github.com>
2019-04-26 14:37:52 -0400

    Add files via upload
    added the No line

---
+++ Packet_Filtering.py
@@ -0,0 +1,44 @@
+import sys, os
+
+def read(f):
+        with open(f) as flie:
+                for i, l in enumerate(flie):
+                        pass
+        size = i + 1
+        h = 0
+
+        f = open(f,'r')
+        line = f.readline().strip()
+        No = line
+	
+
+        space = "\n"
+        clue = open('goodstuff.txt','w')
+        clue.write(space)
+
+        while h < size:
+                if(line.find("No.") == 0):
+                        line = f.readline().strip()
+                        if(line.find("Echo (ping) reply") != -1 or line.find("Echo (ping) request") != -1):
+                                g = 0
+				clue.write(No+space)
+                                while True:
+                                        clue.write(line+space)
+                                        line = f.readline().strip()
+                                        if(len(line) == 0 and g > 1):
+                                                clue.write(space)
+                                                break
+                                        g += 1
+                line = f.readline().strip()
+
+                #print line
+                h += 1
+
+
+
+
+
+hold = sys.argv[1]
+
+read(hold)
+
